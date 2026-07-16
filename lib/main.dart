482fbbd36d1e0cc718f9f20d5370627015419c4b
taposroy306919-collab <taposroy306919@gmail.com>
2026-07-16 23:40:45 +0600

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:project_1/screen/splashscreen.dart';
 
 void main(){
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Splashscreen(),
-    )
-  );
+    );
+  }
 }
